5c6223259832f8e1b40559553bcb3f82dad6bd89
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2023-08-05 22:23:03 +0530

    Update pyramid_1.py

--- pyramid_1.py
+++ pyramid_1.py
@@ -5,7 +5,7 @@ for i in range(0,n):
     for j in range(0,i+1):
         print(i,end="")
     print("")
-/*
+"""
           0
          11
         222
@@ -15,4 +15,4 @@ for i in range(0,n):
     6666666
    77777777
   888888888
-*/
+  """
