20e452a9b1d8d04f87f21d99ed1daa326b046197
aankitsatpathy <113981163+aankitsatpathy@users.noreply.github.com>
2023-08-05 22:22:41 +0530

    Create pyramid_4.py

---
+++ pyramid_4.py
@@ -0,0 +1,13 @@
+k=0
+for i in range(0,5):
+    k+=1
+    for j in range(0,i+1):
+        print(k,end="  ")
+        k+=1
+    print("")
+"""
+1  
+3  4  
+6  7  8  
+10  11  12  13  
+15  16  17  18  19 """
